8d69941acc8bc9d9f8271d9043d70a1f54ebd5ef
Robert Muth <robert@muth.org>
2019-06-08 13:16:37 +0100

    final resolution

--- web/meshes.dart
+++ web/meshes.dart
@@ -9,7 +9,7 @@ import 'logging.dart';
 
 // The final version will set magicMutl to 2 but this is slow
 // to start up so during development it will be 1
-const int magicMult = 4;
+const int magicMult = 8;
 const double kRadius = 125.0 * magicMult;
 const double kHeightScale = 1.0;
 const double kTubeRadius = 25.0 * magicMult;
