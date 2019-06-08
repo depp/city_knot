6060369ba5ccc9267bad42d938fded600379686c
Dietrich Epp <depp@zdome.net>
2019-06-08 12:38:08 -0400

    make sky shader use camera

--- web/sky.dart
+++ web/sky.dart
@@ -3,27 +3,30 @@ library sky;
 import 'package:chronosgl/chronosgl.dart';
 
 void RegisterShaderVars() {
-  IntroduceNewShaderVar('vScreenPosition', ShaderVarDesc(VarTypeVec2, ''));
+  IntroduceNewShaderVar('vRayDirection', ShaderVarDesc(VarTypeVec3, ''));
 }
 
 final ShaderObject VertexShader = ShaderObject("SkyV")
-    ..AddAttributeVars([aPosition, aTexUV])
-    ..AddVaryingVars(['vScreenPosition'])
-    ..SetBody([
-      '''
- void main() {
-     vScreenPosition = ${aTexUV};
-     gl_Position = vec4(${aTexUV} * 2.0 - 1.0, 0.9999, 1.0);
- }
-      '''
-    ]);
-
-final ShaderObject FragmentShader = ShaderObject("SkyF")
-    ..AddVaryingVars(['vScreenPosition'])
-    ..SetBody([
-      '''
+  ..AddAttributeVars([aPosition, aTexUV])
+  ..AddVaryingVars(['vRayDirection'])
+  ..AddUniformVars([uPerspectiveViewMatrix])
+  ..SetBody([
+    '''
 void main() {
-    oFragColor = vec4(vScreenPosition, 0.0, 1.0);
+    mat3 invcamera = inverse(mat3(uPerspectiveViewMatrix));
+    vec3 clippos = vec3(aTexUV * 2.0 - 1.0, 0.9999); 
+    vRayDirection = invcamera * clippos;
+    gl_Position = vec4(clippos, 1.0);
 }
       '''
-    ]);
+  ]);
+
+final ShaderObject FragmentShader = ShaderObject("SkyF")
+  ..AddVaryingVars(['vRayDirection'])
+  ..SetBody([
+    '''
+void main() {
+    oFragColor = vec4(normalize(vRayDirection) * 0.5 + 0.5, 1.0);
+}
+      '''
+  ]);
